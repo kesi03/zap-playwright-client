3b5e9610c5f79611db909e2ce114fbebc3ae7a11
kester simm <kesi03@icloud.com>
2026-05-23 18:06:23 +0100

    API: log exceptions in handleApiAction and return INTERNAL_ERROR

--- src/main/java/org/zaproxy/addon/playwrightclient/PlaywrightClientApi.java
+++ src/main/java/org/zaproxy/addon/playwrightclient/PlaywrightClientApi.java
@@ -3,7 +3,12 @@ package org.zaproxy.addon.playwrightclient;
 import org.zaproxy.zap.extension.api.*;
 import net.sf.json.JSONObject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PlaywrightClientApi extends ApiImplementor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlaywrightClientApi.class);
 
     private static final String PREFIX = "playwrightclient";
     private static final String ACTION_RUN = "runCrawlAndScan";
@@ -24,8 +29,13 @@ public class PlaywrightClientApi extends ApiImplementor {
     public ApiResponse handleApiAction(String name, JSONObject params) throws ApiException {
         if (ACTION_RUN.equals(name)) {
             String url = params.getString("url");
-            ext.runCrawlAndScan(url);
-            return new ApiResponseElement("result", "started");
+            try {
+                ext.runCrawlAndScan(url);
+                return new ApiResponseElement("result", "started");
+            } catch (Exception e) {
+                LOGGER.error("API action runCrawlAndScan failed", e);
+                throw new ApiException(ApiException.Type.INTERNAL_ERROR, e.getMessage());
+            }
         }
 
         throw new ApiException(ApiException.Type.BAD_ACTION);
